c5db7a92d5f3b49f75a6c855bbd7b7efe9da0a8d
niallsiow@yahoo.co.uk <niallsiow@yahoo.co.uk>
2020-12-18 11:49:13 +0000

    added score UI

---
+++ Ski-Jump_Arcade/Assets/UI_Distance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Distance : MonoBehaviour
+{
+
+    public Text currentScore;
+
+    float startingPosition;
+    float currentPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = transform.position.x - startingPosition;
+        currentPosition = Mathf.RoundToInt(currentPosition);
+        currentScore.text = "Score: " + currentPosition.ToString();
+    }
+}
