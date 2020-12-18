eb1a28c6321258294e25548714169d6d85711bc1
niallsiow@yahoo.co.uk <niallsiow@yahoo.co.uk>
2020-12-18 11:05:00 +0000

    moved gitignore, started prototyping rigidbody movement

---
+++ Ski-Jump_Arcade/Assets/PlayerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    Rigidbody myRigidbody;
+
+    float forceAmount = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 force = transform.forward * forceAmount;
+
+        if (Input.GetButtonDown("Fire1")) {
+            myRigidbody.AddForce(force, ForceMode.Impulse);
+        }
+    }
+}
